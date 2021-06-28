8ac2369448662ef821e78a17ff40fca672c161a6
lsg <ehswn6767@naver.com>
2021-06-29 02:52:04 +0900

    selenium 정리완료

--- src/test/java/com/example/spring/SeleniumTest.java
+++ src/test/java/com/example/spring/SeleniumTest.java
@@ -1,6 +1,5 @@
 package com.example.spring;
 
-import org.jsoup.Jsoup;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
@@ -8,8 +7,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-import javax.lang.model.util.Elements;
-import javax.swing.text.Document;
 import java.io.File;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
@@ -20,7 +17,7 @@ import java.util.List;
 public class SeleniumTest {
 
     @Test
-    public void testOpen()throws Exception {
+    public void testOpen() throws Exception {
 
         WebDriver driver = new ChromeDriver();
         // 사이트 에서 원하는데로 이동이 가능하다 그리고 데이터를 받아올수있다
@@ -58,9 +55,9 @@ public class SeleniumTest {
     }
 
     @Test
-    public void testSave2()throws Exception {
+    public void testSave2() throws Exception {
 
-        String path ="https://image-comic.pstatic.net/webtoon/20853/50/20200221113351_d37ebf82b5f17380e3d763379f0b1a51_IMAG01_1.jpg";
+        String path = "https://image-comic.pstatic.net/webtoon/20853/50/20200221113351_d37ebf82b5f17380e3d763379f0b1a51_IMAG01_1.jpg";
         //이미지 절대경로
         URL url = new URL(path);
         HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
@@ -70,7 +67,7 @@ public class SeleniumTest {
         urlConnection.setDoInput(true);
         urlConnection.setUseCaches(false);
 
-        urlConnection.setRequestProperty("user-agent","Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36");
+        urlConnection.setRequestProperty("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36");
         // f12 network 파일클릭 user agent , "해당파일내용"
         InputStream in = urlConnection.getInputStream();
 
@@ -82,7 +79,26 @@ public class SeleniumTest {
     }
 
 
-//    @Test
+    @Test
+    //bad code
+    public void testSave() throws Exception {
+        // 보안이 없을경우 간단한 코드
+
+        String path = "https://image-comic.pstatic.net/nas/user_contents_data/challenge_comic/2021/06/28/fa3030/upload_3473738102062789987.jpeg";
+
+        URL url = new URL(path);
+
+        //read
+        InputStream in = url.openStream();
+
+        File fos = new File("D:\\zzz\\sample3.jpg");
+
+
+        Files.copy(in, fos.toPath());
+
+    }
+
+    //    @Test
 //    public void test1()throws Exception {
 //
 //        System.out.println("Test1................");
@@ -99,25 +115,5 @@ public class SeleniumTest {
 //            System.out.println(title);
 //        });
 //    }
-
-    @Test
-    //bad code
-    public void testSave() throws Exception {
-        //실제 이미지 경로
-        //String path = "https://thumbnail9.coupangcdn.com/thumbnails/remote/230x230ex/image/retail/images/81447876542865-b2de1636-9d42-4236-be96-aad9ff331eb2.jpg";
-
-        String path ="https://image-comic.pstatic.net/nas/user_contents_data/challenge_comic/2021/06/28/fa3030/upload_3473738102062789987.jpeg";
-
-        URL url = new URL(path);
-
-        //read
-        InputStream in = url.openStream();
-
-        File fos = new File("D:\\zzz\\sample3.jpg");
-
-
-        Files.copy(in, fos.toPath());
-
-    }
 
 }
